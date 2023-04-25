2e5f10986b2c12cbb9effa2cd937c7c9edaede3c
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-25 15:29:36 -0500

    Intro To MongoDB Lessons finished

---
+++ Intro_To_MongoDB_And_Mongoose/server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+const path = require('path'),
+  { logger } = require('./middleware/logEvents'),
+  errorHandler = require('./middleware/errorHandler'),
+  PORT = process.env.PORT || 3500,
+  express = require('express'),
+  app = express(),
+  cors = require('cors'),
+  corsOptions = require('./config/corsOptions'),
+  mongoose = require('mongoose'),
+  connectDB = require('./config/dbConn');
+
+//Connect to MongoDB
+connectDB();
+
+// Custom Middleware Logger
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type' : application/x-www-form.urlencoded'
+// is to handle url form data, when is submitted
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employees'));
+
+// app.use('/')
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+  console.log(`Connected to MongoDB`);
+  app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+  });
+});
